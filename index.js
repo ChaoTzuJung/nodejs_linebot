2f6cbf2e26a30c75a8027b0d10ece2b5a5763df1
Alan Chao <s110319022@stu.ntue.edu.tw>
2018-04-19 14:52:36 +0800

    add PM2.5 feature with linebot

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const linebot = require('linebot');
-const express = require('express');
+var express = require('express');
+var getJSON = require('get-json');
+
+var timer;
+var pm = [];
+getJSON_bot();
+
+PM25_bot();
 
 //建立linebot物件
 const bot = linebot({
@@ -25,27 +32,77 @@ app.post('/linewebhook', linebotParser);
 //處理訊息事件
 bot.on('message', function (event) {
   //回覆收到的訊息文字
-	event.reply(event.message.text).then(function (data) {
-    // 處理回覆成功的程式碼
-		console.log('Success', data);
-	}).catch(function (error) {
-    // 處理回覆失敗的程式碼
-		console.log('Error', error);
-	});
+  event.reply(event.message.text)
+    .then(function (data) {
+      // 處理回覆成功的程式碼
+		  console.log('Success', data);
+    })
+    .catch(function (error) {
+      // 處理回覆失敗的程式碼
+		  console.log('Error', error);
+	  });
 });
 //重複我們說過的話
 bot.on('message', function(event) {
   if (event.message.type = 'text') {
     var msg = event.message.text;
-    event.reply(msg).then(function(data) {
-      // success 
+    event.reply(msg)
+    .then(function(data) {
+      setTimeout(function(){
+        var userId = 'ohmygad5987326';
+        var sendMsg = '我正在學習你說話';
+        bot.push(userId,sendMsg);
+        console.log('send: '+sendMsg);
+      },5000);
       console.log(msg);
-    }).catch(function(error) {
+    })
+    .catch(function(error) {
       // error 
       console.log('error');
     });
   }
 });
+
+function PM25_bot() {
+  bot.on('message', function(event) {
+    if (event.message.type == 'text') {
+      var msg = event.message.text;
+      var replyMsg = '';
+      if (msg.indexOf('PM2.5') != -1) {
+        pm.forEach(function(e, i) {
+          if (msg.indexOf(e[0]) != -1) {
+            replyMsg = e[0] + '的 PM2.5 數值為 ' + e[1];
+          }
+        });
+        if (replyMsg == '') {
+          replyMsg = '請輸入正確的地點';
+        }
+      }
+      if (replyMsg == '') {
+        replyMsg = '不知道「'+msg+'」是什麼意思 :p';
+      }
+
+      event.reply(replyMsg).then(function(data) {
+        console.log(replyMsg);
+      }).catch(function(error) {
+        console.log('error');
+      });
+    }
+  });
+}
+
+function getJSON_bot() {
+  clearTimeout(timer);
+  getJSON('http://opendata2.epa.gov.tw/AQX.json', function(error, response) {
+    response.forEach(function(e, i) {
+      pm[i] = [];
+      pm[i][0] = e.SiteName;
+      pm[i][1] = e['PM2.5'] * 1;
+      pm[i][2] = e.PM10 * 1;
+    });
+  });
+  timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
+}
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 // var server = app.listen(process.env.PORT || 8080, function() {
